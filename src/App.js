02c75df88a0313839df498c0d4da2ea0896aa7f8
perdomo2222 <kiuby2608@gmail.com>
2023-05-24 11:19:35 -0600

    El proyecto frontend

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ListaLibros from './ListaLibros';
+import AgregarLibros from './AgregarLibro';
+import Nav from './Nav';
 import './App.css';
+import EditarLiro from './EditarLibro'
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<ListaLibros />} />
+            <Route path="/agregar" element={<AgregarLibros />} />
+            <Route path="/editar/:id" element={<EditarLiro />} />
+          </Routes>
+        </BrowserRouter>
       </header>
     </div>
   );
---
+++ src/Nav.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Nav = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container">
+                <a className="navbar-brand" href="/">Inicio</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                        <a className="nav-link" href="/agregar">Agregar Libro</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
